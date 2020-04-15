db2da76acf29badb2e8a29c76c98ed3a544061e2
Thomas Hessburg <44884308+TomHessburg@users.noreply.github.com>
2020-04-15 00:58:58 -0400

    created convention for registering services

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { postRoutes } from "./services/postsService/PostRoutes";
+import { registerService } from "./services/registerService";
 /**
  * import all routes from a service, give them a baseRoute. All base routes should be unique
  */
+
 export const AppRoutes = [
-  {
-    serviceName: "posts",
-    routes: postRoutes,
-  },
+  // registerService returns an object thats simply
+  // {
+  //   serviceName: "posts",
+  //   routes: postRoutes,
+  // },
+  registerService("posts", postRoutes),
 ];
---
+++ src/services/registerService.ts
@@ -0,0 +1,8 @@
+/**
+ * Function used to register services to routes
+ */
+
+// TODO: correctly type routes:
+export const registerService = (serviceName: string, routes: any) => {
+  return { serviceName, routes };
+};
